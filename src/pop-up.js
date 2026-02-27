d8751ae5ec1c438d816fce30dd3af863299e5736
Olivia Mckelvey <olivia.r.mckelvey@gmail.com>
2026-02-27 19:33:55 +0000

    intial commit

---
+++ src/pop-up.js
@@ -0,0 +1,34 @@
+//BREAK DOWN BUTTON
+//create an HTML element that will serve as our breakdown button 
+const startButton = document.createElement("button")
+//give the button an id so we can reference it in CSS file
+startButton.id = "breakdown"
+//populate the button with some text
+startButton.innerText = "Break Down Imminent"
+//append the element to the document - ARE WE APPENDING TO DOCUMENT OR SOMETHING ELSE
+document.body.append(startButton)
+
+
+//EXIT BUTTON
+//create an HTML element that will serve as our breakdown button 
+const endButton = document.createElement("button")
+//give the button an id so we can reference it in CSS file
+endButton.id = "calm"
+//populate the button with some text
+endButton.innerText = "Cleared Feelings Cache"
+//append the element to the document - ARE WE APPENDING TO DOCUMENT OR SOMETHING ELSE
+document.body.append(endButton)
+
+
+
+//what do we want to do when the start button is pressed?
+//change the wed browsers appearance to be calmer (maybe even rain calming emojis)
+//play a soothing song/sound (need to access an API?)
+//randomly generate an encouraging/calming quote (definitely need an API)
+
+//
+
+
+//add an event listener to the button element calling all functions pertaining to the event
+
+
