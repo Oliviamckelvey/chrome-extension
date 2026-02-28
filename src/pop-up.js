58c1c4b9787efd0a5f10c9e5823b0f09e917e29d
Olivia Mckelvey <olivia.r.mckelvey@gmail.com>
2026-02-28 17:20:44 +0000

    start of saturday

--- src/pop-up.js
+++ src/pop-up.js
@@ -1,35 +1,31 @@
-//BREAK DOWN BUTTON
-//create an HTML element that will serve as our breakdown button 
-const startButton = document.createElement("button")
-//give the button an id so we can reference it in CSS file
-startButton.id = "breakdownbtn"
-//populate the button with some text
-startButton.innerText = "Break Down Imminent"
-//append the element to the document - ARE WE APPENDING TO DOCUMENT OR SOMETHING ELSE
-document.body.append(startButton)
+//START button event listener
+//create a reference variable for the breakdownbtn - accessing it from HTML by ID
+const breakdownButton = document.querySelector('.breakdownbtn')
 
+//add an event listener to watch for a "click" and respond with an async function that will handle message passing
+breakdownButton.addEventListener("click", async () => {
+    //create a reference to the current tab of the browser
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
 
-//EXIT BUTTON
-//create an HTML element that will serve as our breakdown button 
-const endButton = document.createElement("button")
-//give the button an id so we can reference it in CSS file
-endButton.id = "calmbtn"
-//populate the button with some text
-endButton.innerText = "Cleared Feelings Cache"
-//append the element to the document - ARE WE APPENDING TO DOCUMENT OR SOMETHING ELSE
-document.body.append(endButton)
+    //send a message (an object of structured information ) to the current broswer using its ID
+    chrome.tabs.sendMessage(tab.id, { action: "breakdown" })
+})
 
 
 
-//what do we want to do when the start button is pressed?
-//change the wed browsers appearance to be calmer (maybe even rain calming emojis)
-//play a soothing song/sound (need to access an API?)
-//randomly generate an encouraging/calming quote (definitely need an API)
+//STOP button event listener
+//create a reference variable for the calmbtn - accessing it from HTML by ID
+const calmButton = document.querySelector('.calmbtn')
 
-//
+//add an event listener to watch for a "click" and respond with an async function that will handle message passing
+calmButton.addEventListener("click", async () => {
+    //create a reference to the current tab of the browser
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+
+    //send a message (an object of structured information ) to the current browser using its ID
+    chrome.tabs.sendMessage(tab.id, { action: "breakdown" })
+})
 
 
-//add an event listener to the button element calling all functions pertaining to the event
 
 
-npm 
